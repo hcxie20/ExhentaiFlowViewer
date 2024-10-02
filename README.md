b8ce5f7f66301471fe2e2ec04ff543d5d7bb3a76
Haochen <haochenxie@microsoft.com>
2024-10-02 21:11:27 +0800

    Add fetch backup img logic

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Forked from [exhentai flow viewer](https://sleazyfork.org/en/scripts/27056-exhentai-flow-viewer)
 
 This scripts helps you to view a gallery in a single page, from up to down.
+
+## Fixes
+
+### 2024/10/02
+
+With the original extension,if an image failed to load (due to a timeout), the page would be automatically redirected to a backup URL to fetch the image. This process would reload the entire gallery, causing all images to be fetched again. Additionally, the backup URL for the failed image would not be applied in the new round of fetching, leading to repeated failures and endless reloads.
+
+Our forked version addresses this issue by overriding the image fetching failure action. When an image fails to load, the extension automatically fetches the backup image and replaces the timed-out one without reloading the entire gallery. This ensures a seamless and uninterrupted browsing experience, allowing users to enjoy the gallery without unnecessary delays or repeated failures.
--- script.js
+++ script.js
@@ -1,47 +1,88 @@
 // ==UserScript==
 // @name         exhentai flow viewer
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @author       PokemonMaster802
+// @version      1.2
+// @author       MrJohnnY20
 // @match        https://exhentai.org/s/*
 // @description  This scripts helps you to view a gallery in a single page, from up to down.
 // @locale       en
 // ==/UserScript==
- 
-var container;  // The <div> to hold images, intending to inherit it's style.
+
+var container; // The <div> to hold images, intending to inherit it's style.
 var parser;
- 
+
 function getNext(currURL, currDOC) {
     'use strict';
     var nextURL = currDOC.getElementById('next').href;
- 
+
     // The last page has the next url directing to itself.
     if (currURL === nextURL) {
         console.log('Reach the last page');
         return;
     }
- 
+
     // Prepare http request for the next page.
     var nextPage = new XMLHttpRequest();
-    nextPage.onreadystatechange = function() {
-        if (nextPage.readyState == 4) {
-            if (nextPage.status == 200) {
-                var doc = parser.parseFromString(nextPage.responseText, 'text/html');
-                var img = doc.getElementById('img');
-                img.style.paddingTop = '1em';
-                container.append(img);
-                setTimeout(getNext, 500, nextURL, doc);
-            }
-            else {
-                console.log('Failed to fetch ' + nextURL + ': ' + nextPage.status);
-            }
+    nextPage.onreadystatechange = function () {
+        if (nextPage.readyState !== 4) {
+            return;
         }
+
+        if (nextPage.status !== 200) {
+            console.log('Failed to fetch ' + nextURL + ': ' + nextPage.status);
+            return;
+        }
+
+        var doc = parser.parseFromString(nextPage.responseText, 'text/html');
+        var img = doc.getElementById('img');
+
+        const originalImgOnError = img.getAttribute('onerror');
+
+        // Originally a method 'nl' (in ehg_show.c.js) will be called on image fetch fail to change the url with a query `?nl=xxxx-xxxx`. This uses a backup server with different img URL.
+        // Stop ehg_c from refreshing the page and use the backup img URL directly.
+        img.onerror = () => {
+            // This will cause an error log in browser console.
+            console.log('Error loading img. Try uses backup img instead.');
+            const regex = /'([^']*)'/g;
+            let suffix = '';
+            try {
+                suffix = regex.exec(originalImgOnError)[1];
+            } catch (e) {
+                console.log(
+                    'Extract the nl parameter fails. Loading backup image fails.'
+                );
+                return;
+            }
+            const backupPageURL = nextURL + '?nl=' + suffix;
+
+            // Fetch backup img url.
+            const backupPage = new XMLHttpRequest();
+            backupPage.onreadystatechange = () => {
+                if (backupPage.readyState !== 4 || backupPage.status !== 200) {
+                    return;
+                }
+
+                const backupImgURL = parser
+                    .parseFromString(backupPage.responseText, 'text/html')
+                    .getElementById('img')
+                    .getAttribute('src');
+
+                img.src = backupImgURL;
+            };
+            backupPage.open('GET', backupPageURL, false);
+            backupPage.send();
+        };
+
+        img.style.paddingTop = '1em';
+        container.append(img);
+        setTimeout(getNext, 500, nextURL, doc);
     };
-    nextPage.open("GET", nextURL, false);
+
+    nextPage.open('GET', nextURL, false);
     nextPage.send();
 }
- 
-(function() {
+
+(function () {
     parser = new DOMParser();
     container = document.getElementById('i3');
     getNext(document.URL, document);
